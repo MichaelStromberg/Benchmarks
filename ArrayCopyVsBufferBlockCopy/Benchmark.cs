e1bc76abc21955d9d674531dce0de6c4758b5b52
Michael Stromberg <mstromberg@illumina.com>
2019-05-30 12:27:20 -0700

    Added Array.Copy vs Buffer.BlockCopy

---
+++ ArrayCopyVsBufferBlockCopy/Benchmark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ArrayCopyVsBufferBlockCopy
+{
+    [CoreJob]
+    [RPlotExporter, RankColumn]
+    public class ArrayCopyBenchmark
+    {
+        private readonly byte[] _compressedBlock   = new byte[65536];
+        private readonly byte[] _uncompressedBlock = new byte[65536];
+        private const int CopyLength               = 30000;
+
+        [GlobalSetup]
+        public void Setup() {}
+
+        [Benchmark]
+        public void ArrayCopy() => Array.Copy(_uncompressedBlock, 10, _compressedBlock, 10000, CopyLength);
+
+        [Benchmark]
+        public void BufferBlockCopy() => Buffer.BlockCopy(_uncompressedBlock, 10, _compressedBlock, 10000, CopyLength);
+    }
+
+    public class Program
+    {
+        public static void Main() => BenchmarkRunner.Run<ArrayCopyBenchmark>();
+    }
+}
